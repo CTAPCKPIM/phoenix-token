b95cc888f669ffe09f98811ac43f547f9d90fec8
CTAPCKPIM <novickdanya@gmail.com>
2025-05-30 12:42:32 +0300

    feat(token): implement upgradable mintable/burnable ERC20

---
+++ hardhat.config.ts
@@ -0,0 +1,32 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import '@openzeppelin/hardhat-upgrades';
+import 'solidity-coverage';
+import 'dotenv/config';
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: { 
+        enabled: true, 
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: process.env.SEPOLIA_URL || "",
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      chainId: 11155111,
+    }
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: process.env.ETHERSCAN_API_KEY || "",
+    },
+  },
+};
+
+export default config;
---
+++ test/PhoenixToken.test.ts
@@ -0,0 +1,8 @@
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { expect } from "chai";
+import hre from "hardhat";
+
+describe("PhoenixToken", function () {
+  // todo: add tests for Phoenix token..
+});
